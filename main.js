160f0ebd50eb1e7f08385fcaf32ec75f308489ab
xfelipe <xfelipegd@gmail.com>
2020-12-05 20:14:31 -0300

    Estrutura do projeto corrigida e filter do timer implementado

--- main.js
+++ main.js
@@ -1,39 +1,53 @@
 // Criar arquivo novo pro timer
 // https://github.com/Marco-Andre90/projeto-quiz
 // Procurar algo do Vue ou lib js pra fazer mecanica de passagem de tempo
-class Timer {
-    startTimer = (seccondsToAlarm, waitMessage, finishMessage) => {
-        this.statusMessage = waitMessage;
-        this.remainingTime = seccondsToAlarm;
-        this.timerActiveInterval = setInterval(() => {
-            this.remainingTime -= 1;
-            if (this.remainingTime <= 0) {
-                this.stopTimer();
-                this.statusMessage = finishMessage;
-            }
-        }, 1000);
-    }
 
-    stopTimer = () => {
-        clearTimeout(this.timerActiveInterval);
-    }
-
-    startActivity = () => {
-        //this.startTimer(25 * 60, "Relogio Iniciado", "Relogio Terminado");
-        this.startTimer(3, "Relogio Iniciado", "Relogio Terminado");
-    
-    }
-
-    constructor(minutes) {
-        this.statusMessage = "";
-        //this.startActivity();
-    }
-}
+const MINUTE = 60
 
 var app = new Vue({
     el: '#app',
     data: {
-        timer: new Timer()
+        statusMessage: "",
+        remainingTime: 0,
+        timerActiveInterval: undefined
+    },
+    methods: {
+        startTimer: function (seccondsToAlarm, waitMessage, finishMessage) {
+            if (this.timerActiveInterval != undefined) this.stopTimer(); 
+            this.statusMessage = waitMessage;
+            this.remainingTime = seccondsToAlarm;
+            this.timerActiveInterval = setInterval(() => {
+                this.remainingTime -= 1;
+                if (this.remainingTime <= 0) {
+                    this.stopTimer();
+                    this.statusMessage = finishMessage;
+                }
+            }, 1000);
+        },
+        stopTimer: function () {
+            clearTimeout(this.timerActiveInterval);
+        },
+        startActivity: function () {
+            this.startTimer(25 * MINUTE, "Atividade iniciada", "Atividade chegou ao fim.");
+            //this.startTimer(3, "Atividade iniciada", "Atividade chegou ao fim");
+        },
+        startShortBreak: function() {
+            this.startTimer(5 * MINUTE, "Descanso iniciado", "Vai trabalhaaar vagabundo");
+            //this.startTimer(3, "Descanso iniciado", "Vai trabalhaaar vagabundo");
+        },
+        startLongBreak: function() {
+            this.startTimer(30 * MINUTE, "Descanso longo iniciado", "Descanso longo chegou ao fim");
+            //this.startTimer(3, "Descanso longo iniciado", "Descanso longo chegou ao fim");
+        }
+    },
+    filters: {
+        SeccondsToTimer: function(secconds) {
+            let mins = Math.floor(secconds / MINUTE).toString();
+            let secs = Math.floor(secconds % MINUTE).toString();
+            if (mins.length == 1) mins = '0'+mins;
+            if (secs.length == 1) secs = '0'+secs;
+            return mins + ':' + secs;
+            // CONTINUAR IMPLEMENTANDO FILTER
+        }
     }
 });
-
