230a9456f09a37e6bcd3b201fdc056e463e62db6
xfelipe <xfelipegd@gmail.com>
2020-12-06 16:30:24 -0300

    Renderização condicional de botões start/stop e mensagem de pausa.

--- main.js
+++ main.js
@@ -1,46 +1,78 @@
 // Criar arquivo novo pro timer
 // https://github.com/Marco-Andre90/projeto-quiz
 // Procurar algo do Vue ou lib js pra fazer mecanica de passagem de tempo
+// contador/timer em pausa
 
 const MINUTE = 60
+const ACTIVITY = {
+    time: 25 * MINUTE,
+    waitMessage: "Atividade iniciada",
+    finishMessage: "Atividade chegou ao fim",
+    name: "Atividade"
+}
+const SHORT_BREAK = {
+    time: 5 * MINUTE,
+    waitMessage: "Descanso iniciado",
+    finishMessage: "Vai trabalhaaar vagabundo",
+    name: "Descanso"
+}
+const LONG_BREAK = {
+    time: 30 * MINUTE,
+    waitMessage: "Descanso longo iniciado",
+    finishMessage: "Descanso longo chegou ao fim",
+    name: "Descanso longo"
+}
 
 var app = new Vue({
     el: '#app',
     data: {
         statusMessage: "",
-        remainingTime: 0,
+        currentTimerOption: ACTIVITY,
+        remainingTime: ACTIVITY.time,
         activeInterval: false
     },
+
     methods: {
-        startTimer: function (seccondsToAlarm, waitMessage, finishMessage) {
+        startTimer: function (timerMode) {
             if (this.activeInterval != undefined) this.stopTimer();
-            this.statusMessage = waitMessage;
-            this.remainingTime = seccondsToAlarm;
+            this.currentTimerOption = timerMode;
+            this.remainingTime = timerMode.time;
+            this.resumeTimer();
+        },
+        stopTimer: function() {
+            clearTimeout(this.activeInterval);
+            this.activeInterval = false;
+            this.updateMessage();
+        },
+        resumeTimer: function() {
             this.activeInterval = setInterval(() => {
                 this.remainingTime -= 1;
                 if (this.remainingTime <= 0) {
                     this.stopTimer();
-                    this.statusMessage = finishMessage;
                 }
             }, 1000);
+            this.updateMessage();
         },
-        stopTimer: function () {
-            clearTimeout(this.activeInterval);
-            this.activeInterval = false;
+        updateMessage: function() {
+            if (this.activeInterval) {
+                this.statusMessage = this.currentTimerOption.waitMessage;
+            } else if (this.remainingTime > 0) {
+                this.statusMessage = this.currentTimerOption.name + " em pausa"
+            } else {
+                this.statusMessage = this.currentTimerOption.finishMessage;
+            }
         },
         startActivity: function () {
-            this.startTimer(25 * MINUTE, "Atividade iniciada", "Atividade chegou ao fim.");
-            //this.startTimer(3, "Atividade iniciada", "Atividade chegou ao fim");
+            this.startTimer(ACTIVITY);
         },
         startShortBreak: function() {
-            this.startTimer(5 * MINUTE, "Descanso iniciado", "Vai trabalhaaar vagabundo");
-            //this.startTimer(3, "Descanso iniciado", "Vai trabalhaaar vagabundo");
+            this.startTimer(SHORT_BREAK);
         },
         startLongBreak: function() {
-            this.startTimer(30 * MINUTE, "Descanso longo iniciado", "Descanso longo chegou ao fim");
-            //this.startTimer(3, "Descanso longo iniciado", "Descanso longo chegou ao fim");
+            this.startTimer(LONG_BREAK);
         }
     },
+
     filters: {
         seccondsToTimer: function(secconds) {
             let mins = Math.floor(secconds / MINUTE).toString();
